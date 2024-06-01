ac0143f9e5ba660faa77b11a9a941676bcbe6459
Pavle <pavlevujosevic125@gmail.com>
2024-06-01 14:24:28 +0200

    model 3

---
+++ src/main/java/me/fit/rest/client/IPClient.java
@@ -0,0 +1,13 @@
+package me.fit.rest.client;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import me.fit.model.IPLog;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+@Path("/data")
+@RegisterRestClient
+public interface IPClient {
+	@GET
+	@Path("/client-ip")
+	IPLog getIp();
+}
